ee0d20dd9ec31a078d9b91a171d214e6f7b8550b
Jyke321 <jyke321@gmail.com>
2021-10-22 21:40:27 -0400

    This is my rough draft for a gui

---
+++ ToDoListApplication/src/main/java/baseline/Application.java
@@ -0,0 +1,19 @@
+package baseline;
+
+public class Application {
+    public static void main(String[] args) {
+        /*In case you read this, I know I don't
+        have much of  a design done right now,
+        but I was in a lot of pain earlier this week
+        because what I thought was a tailbone injury turned out be a cist
+        which I didn't figure out until yesterday.
+        So I tried my best to get this much done today,
+        I did attend lectures online, I only made the last
+        30 minutes of yesterday's lecture since that was
+        the time I got back home from the hospital, and I
+        hope now I can get more on tracks since I was initially
+        really excited to mess around with GUIs.
+        Thanks for reading if you saw this.
+         */
+    }
+}
